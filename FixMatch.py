06cf82ae40868d20075558fe23ce1eef42011565
Gustavo Beck <teodoro.beck@gmail.com>
2020-10-14 20:18:15 +0200

    Finished the skeleton of training

--- FixMatch.py
+++ FixMatch.py
@@ -16,6 +16,7 @@ from torch.utils.tensorboard import SummaryWriter
 from data.data_loader import load_cifar10
 import torchvision
 from train import *
+from exp_moving_avg import EMA
 
 
 ###### SET VARIABLES ######
@@ -25,6 +26,7 @@ DATA_ROOT = './data'
 n_labeled_data = 4000 # We will train with 4000 labeled data to avoid computing many times the CTAugment
 B = 64 # B from the paper, i.e. number of labeled examples per batch. 
 mu = 7 # Hyperparam of Fixmatch determining the relative number of unlabeled examples w.r.t. B * mu
+unlabeled_batch_size = B * mu
 warmup_steps = 5 # Define number of warmup steps to avoid premature cyclic learning
 initial_learning_rate = 0.3 # Small learning rate, which with cyclic decay will tend to zero
 momentum = 0.9 # Momentum to access the Stochastic Gradient Descent
@@ -32,6 +34,7 @@ nesterov_factor = False # They found that the nesterov hyperparm wasn't necessar
 pseudo_label_threshold = 0.95 # Threshold to guarantee confidence on the model
 total_training_epochs = 5 # Number of training epochs, without early stopping
 initial_training_epoch = 0 # Start the training epoch from zero
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # Create device to perform computations in GPU (if available)
 
 ###### DEFINE FUNCTIONS ######
 def cyclic_learning_rate_with_warmup(warmup_steps, epoch, total_training_epochs):
@@ -49,9 +52,9 @@ def cyclic_learning_rate_with_warmup(warmup_steps, epoch, total_training_epochs)
 labeled_dataset, unlabeled_dataset, test_dataset = load_cifar10(DATA_ROOT, n_labeled_data)
 
 # Load datasets
-labeled_trainloader = DataLoader(labeled_dataset, sampler = RandomSampler(labeled_dataset), batch_size= B, num_workers= 0, drop_last=True)
-unlabeled_trainloader = DataLoader(unlabeled_dataset, sampler = RandomSampler(unlabeled_dataset), batch_size = B*mu, num_workers = 0, drop_last=True)
-test_loader = DataLoader(test_dataset, sampler = SequentialSampler(test_dataset), batch_size= B, num_workers= 0)
+labeled_train_data = DataLoader(labeled_dataset, sampler = RandomSampler(labeled_dataset), batch_size= B, num_workers= 0, drop_last=True)
+unlabeled_train_data = DataLoader(unlabeled_dataset, sampler = RandomSampler(unlabeled_dataset), batch_size = unlabeled_batch_size, num_workers = 0, drop_last=True)
+test_loader = DataLoader(test_dataset, sampler = SequentialSampler(test_dataset), batch_size = B, num_workers= 0)
 
 '''def imshow(img):
     img = img / 2 + 0.5
@@ -61,7 +64,7 @@ test_loader = DataLoader(test_dataset, sampler = SequentialSampler(test_dataset)
 
 
 # show images of unlabeled data
-for i, (images, labels) in enumerate(unlabeled_trainloader):
+for i, (images, labels) in enumerate(unlabeled_train_data):
     print(i)
     print('Number of labels ', len(labels))
     print('Weakly and Strongly? ', len(images))
@@ -76,17 +79,50 @@ model = None
 # Define Stochastic Gradient Descent 
 optimizer = optim.SGD(model.parameters(), lr=initial_learning_rate, momentum=momentum, nesterov=nesterov_factor)
 
-# Create scheduler that will take charge of warming up and learning rate decay
+# Create scheduler that will take charge of warming up and performing learning rate decay
+# LambdaLR: Sets the learning rate of each parameter group to the initial lr times a given function. (Pytorch documentation)
 scheduler = LambdaLR(optimizer, lr_lambda = cyclic_learning_rate_with_warmup(warmup_steps, initial_training_epoch, total_training_epochs))
 
-# Initialize training
-model.zero_grad()
+# Define exponential moving avarage of the parameters with 0.999 weight decay
+exp_moving_avg = EMA(model.parameters(), decay=0.999)
+
+# Analyze the training process
+acc_model = []
+acc_ema = []
+
 for epoch in range(total_training_epochs):
-    # Train model, update weights in each batch based on the combination of labeled and unlabeled losses
-    model = train_fixmatch()
+    # Initialize training
+    model.zero_grad().to(device)
+    model.train()
 
-    # Test
-    accuracy = test_fixmatch()
+    # Current learning rate to compute the loss combination
+    lr = cyclic_learning_rate_with_warmup(warmup_steps, epoch, total_training_epochs)
 
+    # Train model, update weights per epoch based on the combination of labeled and unlabeled losses
+    semi_supervised_loss, supervised_loss, unsupervised_loss = train_fixmatch(
+                                                                            model,
+                                                                            device,
+                                                                            labeled_train_data,
+                                                                            unlabeled_train_data,
+                                                                            lr,
+                                                                            B,
+                                                                            unlabeled_batch_size,
+                                                                            pseudo_label_threshold
+                                                                            )
+
+    # Update the weights
+    semi_supervised_loss.backward()
+
+    # Update learning rate, optimizer (SGD), and exponential moving average parameters
+    scheduler.step()
+    optimizer.step()
+    exp_moving_avg.update(model.parameters())
+
+    # Test and compute the accuracy for the current model and exponetial moving average
+    acc_model_tmp, acc_ema_tmp = test_fixmatch(exp_moving_avg, model, test_loader, B)
+
+    # Stack learning process
+    acc_model.extend(acc_model_tmp)
+    acc_ema.extend(acc_ema_tmp)
 
 
--- train.py
+++ train.py
@@ -1,13 +1,108 @@
+import numpy as np
+import torch
+import torch.backends.cudnn as cudnn
+from torch.nn.functional import cross_entropy
+import torch.optim as optim
 
 
-def train_fixmatch():
-    labeled_data_loss = 0
-    unlabeled_data_loss = 0
-    total_loss = 0
+##### TRAINING #####
+def train_fixmatch(model, device, labeled_train_data, unlabeled_train_data, learning_rate, B, unlabeled_batch_size, threshold):
+    # Compute loss for labeled data (mean loss of images)
+    supervised_loss = supervised_train(model, device, labeled_train_data, B)
 
-    return total_loss, labeled_data_loss, unlabeled_data_loss
+    # Compute loss of unlabeled_data (mean loss of images)
+    unsupervised_loss = unsupervised_train(model, device, unlabeled_train_data, unlabeled_batch_size, threshold)
 
-def test_fixmatch():
-    acc = 0
+    # Compute the total loss (SSL loss)
+    semi_supervised_loss = supervised_loss + learning_rate * unsupervised_loss
 
-    return acc
+    return semi_supervised_loss, supervised_loss, unsupervised_loss
+
+def supervised_train(model, device, labeled_train_data, B):
+    loss = []
+    for batch_idx, img_batch in enumerate(labeled_train_data):
+        # Define batch images and labels
+        inputs, targets = img_batch
+
+        # Make predictions
+        predictions = model(inputs.to(device))
+
+        # Compute loss of batch
+        loss.extend(cross_entropy(predictions, targets.to(device), reduction='mean'))
+
+    # Average over the labeled batch
+    supervised_loss = sum(loss) / B
+    return supervised_loss
+
+def unsupervised_train(model, device, unlabeled_train_data, unlabeled_batch_size, threshold):
+    loss = []
+    for batch_idx, img_batch in enumerate(unlabeled_train_data):
+        # Define batch images (weakly and strongly augmented) and not assing the target labels
+        weakly_augment_inputs, strongly_augment_inputs, _ = img_batch
+
+        # Assign Pseudo-labels and mask them based on threshold 
+        pseudo_labels, masked_indeces = pseudo_labeling(model, weakly_augment_inputs.to(device), threshold)
+
+        # Compute predictions for strongly augmented images
+        strongly_predictions = model(strongly_augment_inputs.to(device))
+
+        # Compute loss between pseudo-labeled images and strongly augmented images
+        masked_loss = cross_entropy(strongly_predictions, pseudo_labels, reduction='mean')[masked_indeces]
+        loss.extend(masked_loss)
+        
+    # Average over the unlabeled batch
+    unsupervised_loss = sum(loss) / unlabeled_batch_size
+    return unsupervised_loss
+
+def pseudo_labeling(model, weakly_augment_inputs, threshold):
+    all_probs = []
+    # Detach the linear prediction 
+    logits = model(weakly_augment_inputs.detach())
+
+    # Compute pseudo probabilities
+    probs = torch.softmax(logits, dim=1)
+
+    # One hot encode pseudo labels and define mask for those that surpassed the threshold
+    scores, pseudo_labels = torch.max(probs, dim=1)
+    masked_indeces = scores >= threshold
+    return pseudo_labels, masked_indeces
+
+##### TESTING #####
+def test_fixmatch(ema, model, test_data, B):
+    # Compute accuract for the model and ema
+    acc_model_tmp = [] # Creating a list might be interesting if you decide to check the performance of each batch
+    acc_ema_tmp = []
+    for batch_idx, img_batch in enumerate(test_data):
+        # Define batch images and labels
+        inputs, targets = img_batch
+        
+        # Evalutate method for the model 
+        model.eval()
+        logits = model(inputs)
+        acc_model_tmp.extend(evaluate(logits, targets))
+
+        # Evalutate method for the ema
+        ema.copy_to(model.parameters())
+        logits = model(inputs)
+        acc_ema_tmp.extend(evaluate(logits, targets))
+
+    # Compute the accuracy average over the batches (size B) 
+    acc_model = sum(acc_model_tmp) / B
+    acc_ema = sum(acc_ema_tmp) / B
+
+    return acc_model, acc_ema
+
+def evaluate(logits, targets):
+    # Compute the scores
+    scores = torch.softmax(logits, dim=1)
+
+    # Return the predictions
+    _, preds = torch.max(scores, dim=1)
+    
+    # Compare the test labels to the predictions
+    match = (targets == preds)
+
+    # Compute accuracy by comparing correct and wrong predictions
+    accuracy = torch.mean(match)
+
+    return accuracy
