TODO: Fix learning rate with scheduler. Perform the proper PyTorch function for loss computation.

FixMatch.py
@@ -1,10 +1,12 @@
 import random
+import numpy as np
 import matplotlib.pyplot as plt
 import torch.backends.cudnn as cudnn
 from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data import DataLoader, RandomSampler, SequentialSampler
 from data.data_loader import load_cifar10
 import torchvision
+import torch.optim as optim
 from train import *
 from exp_moving_avg import EMA
 from WideResNet_PyTorch.src import WideResNet as wrn
@@ -25,10 +27,10 @@ def set_seed(seed=42):
 def cyclic_learning_rate_with_warmup(warmup_steps, epoch, total_training_epochs):
     # If you don't achieve the number of warmup steps, don't update
     if epoch < warmup_steps:
-        new_learning_rate_multiplicative = 1
+        new_learning_rate_multiplicative = lambda x: 1
     else:  # Once you surpass the number of warmup steps,
         # you should decay they learning rate close zero in a cosine manner
-        new_learning_rate_multiplicative = np.cos(7. / 16. * np.pi * (epoch / total_training_epochs))
+        new_learning_rate_multiplicative = lambda x: np.cos(7. / 16. * np.pi * (epoch / total_training_epochs))
     # Update learning rate scheduler
     return new_learning_rate_multiplicative
 
@@ -86,7 +88,7 @@ def main():
 
     # Create Wide - ResNet based on the data set characteristics
     model = wrn.WideResNet(d=wrn_depth, k=wrn_width, n_classes=n_classes, input_features=channels,
-                           output_features=n_classes, strides=strides)
+                           output_features=16, strides=strides)
 
     # Define Stochastic Gradient Descent
     optimizer = optim.SGD(model.parameters(), lr=initial_learning_rate, momentum=momentum, nesterov=nesterov_factor)
@@ -94,8 +96,9 @@ def main():
     # Create scheduler that will take charge of warming up and performing learning rate decay
     # LambdaLR: Sets the learning rate of each parameter group to the initial lr times a given function.
     # (Pytorch documentation)
-    scheduler = LambdaLR(optimizer, lr_lambda=cyclic_learning_rate_with_warmup(warmup_steps, initial_training_epoch,
-                                                                               total_training_epochs))
+    scheduler = LambdaLR(optimizer=optimizer, lr_lambda=cyclic_learning_rate_with_warmup(warmup_steps,
+                                                                                         initial_training_epoch,
+                                                                                         total_training_epochs))
 
     # Define exponential moving avarage of the parameters with 0.999 weight decay
     exp_moving_avg = EMA(model.parameters(), decay=0.999)
@@ -107,7 +110,7 @@ def main():
     for epoch in range(total_training_epochs):
         # Initialize training
         model.to(device)
-        model.zero_grad()  # .to(device)
+        model.zero_grad()
         model.train()
 
         # Current learning rate to compute the loss combination
@@ -119,7 +122,7 @@ def main():
             device,
             labeled_train_data,
             unlabeled_train_data,
-            lr,
+            lr(1),
             B,
             unlabeled_batch_size,
             pseudo_label_threshold

WideResNet_PyTorch/src/WideResNet.py
@@ -130,17 +130,17 @@ class WideResNet(nn.Module):
         return self.fc(out), attention1, attention2, attention3
 
 
-if __name__ == '__main__':
-
-    # change d and k if you want to check a model other than WRN-40-2
-    d = 40
-    k = 2
-    strides = [1, 1, 2, 2]
-    net = WideResNet(d=d, k=k, n_classes=10, input_features=3, output_features=16, strides=strides)
-
-    # verify that an output is produced
-    sample_input = torch.ones(size=(1, 3, 32, 32), requires_grad=False)
-    net(sample_input)
-
-    # Summarize model
-    summary(net, input_size=(3, 32, 32))
+# if __name__ == '__main__':
+#
+#     # change d and k if you want to check a model other than WRN-40-2
+#     d = 40
+#     k = 2
+#     strides = [1, 1, 2, 2]
+#     net = WideResNet(d=d, k=k, n_classes=10, input_features=3, output_features=16, strides=strides)
+#
+#     # verify that an output is produced
+#     sample_input = torch.ones(size=(1, 3, 32, 32), requires_grad=False)
+#     net(sample_input)
+#
+#     # Summarize model
+#     summary(net, input_size=(3, 32, 32))

train.py
@@ -1,8 +1,5 @@
-import numpy as np
 import torch
-import torch.backends.cudnn as cudnn
-from torch.nn.functional import cross_entropy
-import torch.optim as optim
+from torch.nn import CrossEntropyLoss
 
 
 ##### TRAINING #####
@@ -18,6 +15,7 @@ def train_fixmatch(model, device, labeled_train_data, unlabeled_train_data, lear
 
     return semi_supervised_loss, supervised_loss, unsupervised_loss
 
+
 def supervised_train(model, device, labeled_train_data, B):
     loss = []
     for batch_idx, img_batch in enumerate(labeled_train_data):
@@ -25,39 +23,56 @@ def supervised_train(model, device, labeled_train_data, B):
         inputs, targets = img_batch
 
         # Make predictions
-        predictions = model(inputs.to(device))
+        predictions = model(inputs.to(device))[0]  # Item 0 -> Output. Items 1, 2, 3 -> Attention
 
         # Compute loss of batch
-        loss.extend(cross_entropy(predictions, targets.to(device), reduction='mean'))
+        criterion = CrossEntropyLoss()
+        loss_tmp = criterion(predictions, targets.to(device))
+        # loss.extend(CrossEntropyLoss(predictions, targets.to(device), reduction='mean'))
+        loss.append(loss_tmp.item())
+
+        break
 
     # Average over the labeled batch
     supervised_loss = sum(loss) / B
     return supervised_loss
 
+
 def unsupervised_train(model, device, unlabeled_train_data, unlabeled_batch_size, threshold):
     loss = []
-    for batch_idx, img_batch in enumerate(unlabeled_train_data):
+    for batch_idx, (image_batch, _) in enumerate(unlabeled_train_data):
         # Define batch images (weakly and strongly augmented) and not assing the target labels
-        weakly_augment_inputs, strongly_augment_inputs, _ = img_batch
+        weakly_augment_inputs, strongly_augment_inputs = image_batch
 
         # Assign Pseudo-labels and mask them based on threshold 
         pseudo_labels, masked_indeces = pseudo_labeling(model, weakly_augment_inputs.to(device), threshold)
 
-        # Compute predictions for strongly augmented images
-        strongly_predictions = model(strongly_augment_inputs.to(device))
+        if True not in masked_indeces:
+            loss.append(0)  # Append a 0 if no image surpassed the threshold
+        else:
+            # Compute predictions for strongly augmented images
+            strongly_predictions = model(strongly_augment_inputs.to(device))[0]
+            # Compute loss of batch
+            criterion = CrossEntropyLoss()
+            loss_tmp = criterion(strongly_predictions[masked_indeces], pseudo_labels[masked_indeces].to(device))
+            # loss.extend(CrossEntropyLoss(predictions, targets.to(device), reduction='mean'))
+            loss.append(loss_tmp.item())
+
+        break
 
         # Compute loss between pseudo-labeled images and strongly augmented images
-        masked_loss = cross_entropy(strongly_predictions, pseudo_labels, reduction='mean')[masked_indeces]
-        loss.extend(masked_loss)
+        # masked_loss = cross_entropy(strongly_predictions, pseudo_labels, reduction='mean')[masked_indeces]
+        # loss.extend(masked_loss)
         
     # Average over the unlabeled batch
     unsupervised_loss = sum(loss) / unlabeled_batch_size
     return unsupervised_loss
 
+
 def pseudo_labeling(model, weakly_augment_inputs, threshold):
     all_probs = []
     # Detach the linear prediction 
-    logits = model(weakly_augment_inputs.detach())
+    logits = model(weakly_augment_inputs.detach())[0]
 
     # Compute pseudo probabilities
     probs = torch.softmax(logits, dim=1)